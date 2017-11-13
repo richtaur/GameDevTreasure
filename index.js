aff5aacab9074298556991d44e26192b731583de
Matt Hackett <richtaur@gmail.com>
2017-11-13 12:06:02 -0800

    update meta values

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ handlebars.registerPartial("footer", footerContents)
 Metalsmith(__dirname)
 	.metadata({
 		siteName: "Game Dev Treasure",
-		description: "Valuable articles and videos about game development, handpicked by obsessive indie game developer Matt Hackett.",
+		description: "Shiny articles and videos about game development, handpicked by obsessive game developer Matt Hackett.",
 		url: URL,
 		date: new Date()
 	})
